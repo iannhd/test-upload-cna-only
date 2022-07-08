467412906d0217bd2e2f567ab6008727d996a8aa
iannhd <in.hadyan@gmail.com>
2022-07-08 17:00:55 +0700

    fixing merge error

--- components/register-page/register-page-component.jsx
+++ components/register-page/register-page-component.jsx
@@ -250,33 +250,12 @@ console.log("data", data);
                           })}
                         </Input>
                         </div>
-                        {/* <div className="container">
-                        <form className="form" method="post" onChange={handleOnChange} onSubmit={handleOnSubmit2}>
-                              <input type="file" name="file" />
-                            
-                              <img src={imageSrc} />
-                              
-                              {imageSrc && !uploadData && (
-                                  <button>Upload Files</button>
-                              )}
-
-                              {uploadData && (
-                                <code><pre>{JSON.stringify(uploadData, null, 2)}</pre></code>
-                              )}
-                       </form>
-                       </div> */}
-                        
                         <div method="post" className="mt-3" onChange={handleOnChange}>
-                        <input type="file" name="file" />
-                            
+                          <input type="file" name="file" />
                             <img src={imageSrc} width="100" height="100"/>
-                            
-                            
-
                             {uploadData && (
                               <code><pre>{JSON.stringify(uploadData, null, 2)}</pre></code>
                             )}
-
                         </div>
                         <div>
                         <p>Already have an account ?  <span>
